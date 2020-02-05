644728acd9cae7ebe14b8e0b79177b638261aceb
DodiyaParth <parthdodiya999@gmail.com>
2020-02-05 15:07:23 +0530

    Translate shapes

--- 2_rotate.c
+++ 2_rotate.c
@@ -5,7 +5,6 @@
 #include<GL/glut.h> 
 #include<math.h> 
 #define pi 3.142857 
-#define S_CIRCLE 0
 #define S_POLYGON 1
 #define S_LINE 2
 
@@ -29,7 +28,7 @@ void myInit (void)
 	gluOrtho2D(-780, 780, -420, 420); 
 }
 
-void circle(int cx,int cy,int r,int tx,int ty)
+/*void circle(int cx,int cy,int r,int tx,int ty)
 {
 	glBegin(GL_POINTS); 
 	float x, y, i;
@@ -40,53 +39,64 @@ void circle(int cx,int cy,int r,int tx,int ty)
 		glVertex2i(x+tx, y+ty); 
 	} 
 	glEnd();
-}
+}*/
+float angle,a_cos,a_sin;
 
-void polygon(int* x,int* y,int n,int tx,int ty)
+void polygon(int* x,int* y,int n,int tx,int ty,float angle)
 {
-	glBegin(GL_POLYGON);
 	int i;
+	glColor3f(0.3, 0.0, 0.1);
+	glBegin(GL_POLYGON);
 	for(i=0;i<n;i++)
 	{
-		glVertex2i(x[i]+tx,y[i]+ty);
+		glVertex2i(x[i],y[i]);
+	}
+	glEnd();
+	glColor3f(0.0, 1.0, 0.0);
+	glBegin(GL_POLYGON);
+	int a,b;
+	for(i=0;i<n;i++)
+	{
+		a=x[i]-tx;
+		b=y[i]-ty;
+		glVertex2i(tx+a_cos*a-a_sin*b,ty+a_sin*a+a_cos*b);
 	}
 	glEnd();
 }
 
-void line(int x1,int py1,int x2,int y2,int tx,int ty)
+void line(int x1,int py1,int x2,int y2,int tx,int ty,float angle)
 {
+	glColor3f(0.3, 0.0, 0.1);
 	glBegin(GL_LINES);
-	glVertex2i(x1+tx,py1+ty);
-	glVertex2i(x2+tx,y2+ty);
+	glVertex2i(x1,py1);
+	glVertex2i(x2,y2);
+	glEnd();
+	glColor3f(0.0, 1.0, 0.0);
+	glBegin(GL_LINES);
+	glVertex2i(tx+a_cos*(x1-tx)-a_sin*(py1-ty),ty+a_sin*(x1-tx)+a_cos*(py1-ty));
+	glVertex2i(tx+a_cos*(x2-tx)-a_sin*(y2-ty),ty+a_sin*(x2-tx)+a_cos*(y2-ty));
 	glEnd();
 }
 
-int shape,cx,cy,r,tx,ty,n,x1,x2,py1,y2;
+int shape,tx,ty,n,x1,x2,py1,y2;
+
 int A[100];
 int B[100];
 
 void display (void) 
 { 
 	glClear(GL_COLOR_BUFFER_BIT);
-	if(shape==S_CIRCLE)
+	if(shape==S_POLYGON)
 	{
-		glColor3f(0.3, 0.0, 0.1);
-		circle(cx,cy,r,0,0);
-		glColor3f(0.0, 1.0, 0.0);
-		circle(cx,cy,r,tx,ty);
-	}else if(shape==S_POLYGON)
-	{
-		glColor3f(0.3, 0.0, 0.1);
-		polygon(A,B,n,0,0);
-		glColor3f(0.0, 1.0, 0.0);
-		polygon(A,B,n,tx,ty);
+		polygon(A,B,n,tx,ty,angle);
 	}else if(shape==S_LINE)
 	{
-		glColor3f(0.3, 0.0, 0.1);
-		line(x1,py1,x2,y2,0,0);
-		glColor3f(0.0, 1.0, 0.0);
-		line(x1,py1,x2,y2,tx,ty);
+		line(x1,py1,x2,y2,tx,ty,angle);
 	}
+	glColor3f(0.0, 1.0, 1.0);
+	glBegin(GL_POINTS);
+	glVertex2i(tx,ty);
+	glEnd();
 	
 	 /*
  	circle(50,50,100,-100,100);
@@ -99,20 +109,12 @@ void display (void)
 
 int main (int argc, char** argv) 
 {
-	printf("Choose Shape... {0: Circle, 1: Polygon, 2: Line}\n");
+	printf("Choose Shape... {1: Polygon, 2: Line}\n");
 	scanf("%d",&shape);
 	//-780, 780, -420, 420
 	printf("LIMIT for x-axis : [-780,780]\n");
 	printf("LIMIT for y-axis : [-420,420]\n");
-	if(shape==S_CIRCLE)
-	{
-		printf("Enter coordinates of Circle cx,cy : ");
-		scanf("%d %d",&cx,&cy);
-		printf("Enter radius : ");
-		scanf("%d",&r);
-		printf("Enter translation factors tx,ty : ");
-		scanf("%d %d",&tx,&ty);
-	}else if(shape==S_POLYGON)
+	if(shape==S_POLYGON)
 	{
 		int i;
 		printf("Enter number of points : ");
@@ -122,16 +124,26 @@ int main (int argc, char** argv)
 		{
 			scanf("%d %d",&A[i],&B[i]);
 		}
-		printf("Enter translation factors tx,ty : ");
+		printf("Enter Center of rotation : ");
 		scanf("%d %d",&tx,&ty);
+		printf("Enter Angle of rotation [0,360] : ");
+		scanf("%f",&angle);
+		angle=angle*pi/180;
+		a_cos=cos(angle);
+		a_sin=sin(angle);
 	}else if(shape==S_LINE)
 	{
 		printf("Enter coordinate of POINT-1 : ");
 		scanf("%d %d",&x1,&py1);
 		printf("Enter coordinate of POINT-2 : ");
 		scanf("%d %d",&x2,&y2);
-		printf("Enter translation factors tx,ty : ");
+		printf("Enter Center of rotation : ");
 		scanf("%d %d",&tx,&ty);
+		printf("Enter Angle of rotation [0,360] : ");
+		scanf("%f",&angle);
+		angle=angle*pi/180;
+		a_cos=cos(angle);
+		a_sin=sin(angle);
 	}else{
 		printf("INVALID INPUT!!!\n");
 		return 0;
